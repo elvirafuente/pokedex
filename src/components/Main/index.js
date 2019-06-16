73e50430040da115c8ad95e359c12dc90856c913
Elvira Fuente <hebebicho@gmail.com>
2019-06-16 20:16:01 +0200

    add fetch

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import PokemonList from '../PokemonList'
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: [],
+      isFetching: true,
     }
   }
 
   componentDidMount(){
+    this.fetchService();
+  }
+
+  fetchService(){
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=25';
+    return (
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const { results } = data;
+        for(let i = 0; i < results.length; i++){
+          fetch(results[i].url)
+          .then(responseSinglePoke => responseSinglePoke.json())
+          .then(dataSinglePoke => {
+            console.log(dataSinglePoke);
+            this.setState(prevState => {
+              return {
+                data: [
+                  ...prevState.data,
+                  dataSinglePoke
+                ]
+              }
+            })
+          })
+        }
+        this.setState({
+          isFetching: false,
+        })
+      })
+      
+    )
   }
 
   render(){
     return (
       <main>
-        <h1>Main</h1>
+        {
+          this.state.isFetching 
+          ? <p>loading...</p>
+          : <PokemonList />
+        }
       </main>
     )
   }
---
+++ src/components/PokemonList/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PokemonList (props){
+    return <p>pokemon list</p>
+}
+
+export default PokemonList;
