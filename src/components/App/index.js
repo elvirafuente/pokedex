da66df7d756ef718e810232386868b5680403574
Elvira Fuente <hebebicho@gmail.com>
2019-06-16 19:02:24 +0200

    add main component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+import './styles.scss';
+import Main from '../Main'
 
 function App() {
   return (
     <div className="App">
-      hola mundo
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+  }
+
+  render(){
+    return (
+      <main>
+        <h1>Main</h1>
+      </main>
+    )
+  }
+}
+
+export default Main;
