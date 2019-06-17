75c675f032ed0fb1429923258442320ba584fe11
Elvira Fuente <hebebicho@gmail.com>
2019-06-17 18:57:30 +0200

    add docs folder

---
+++ docs/precache-manifest.4946111fae4b6ab31bb024303731f69d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb185bb1bafeb9ae58618b249a93c3ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ea7c6e29cb511a0615d",
+    "url": "./static/css/main.59b57e32.chunk.css"
+  },
+  {
+    "revision": "a00b5167032b4cf9e61d",
+    "url": "./static/js/2.aa2dfdb1.chunk.js"
+  },
+  {
+    "revision": "4ea7c6e29cb511a0615d",
+    "url": "./static/js/main.2bdf11da.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
